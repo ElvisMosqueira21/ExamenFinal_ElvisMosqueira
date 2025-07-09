df7ba0d2f9bd1a83ccfb92eb85153d8cc1371854
Elvis Mosqueira <N00447800@upn.pe>
2025-07-09 17:38:02 -0500

    Agregar archivos de proyecto.

---
+++ FINAL_EM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblio_EM;
+
+namespace FINAL_EM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
